ce46e8077aabff59fa62555378bdd85f60825e66
Alex Costan <acostan.con@orangetheory.com>
2020-08-14 17:00:28 -0400

    setting up event listeners

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
 var budgetController = (function() {
 
-    // Encapsulated data
-    var x = 23
-    var add = function(a) {
-        return x + a
-    }
-
-    // Method available for public use
-    return {
-        publicTest: function(b) {
-            return add(b)
-        }
-    }
 
 })()
 
@@ -21,15 +9,31 @@ var UIController = (function() {
 
 })()
 
-var controller = (function(budgetCtrl, UICtrl) {
+var appController = (function(budgetCtrl, UICtrl) {
 
-    // Code here
-    var z = budgetCtrl.publicTest(5) // 28
+    var ctrlAddItem = function() {
 
-    return {
-        anotherPublic: function() {
-            console.log(z)
-        }
+        //1. Get the filed input data
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget 
+
+        // 5. Display the budget on the UI
+
+        console.log('It works')
     }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+    document.addEventListener('keypress', function(event) {
+        
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem()
+        }
+
+    })
 
 })(budgetController, UIController)
