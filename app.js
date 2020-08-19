6133f32485c29aedc631a64c301a2c6a8c443f6f
Alex Costan <acostan.con@orangetheory.com>
2020-08-19 08:49:50 -0400

    formatting the budget numbers using string manipulation

--- app.js
+++ app.js
@@ -144,6 +144,30 @@ var UIController = (function () {
     expensesPercLabel: ".item__percentage",
   }
 
+  var formatNumber = function (num, type) {
+    var numSplit, int, dec
+    /*
+      + or - before number
+      exactly 2 decimal points
+      comma separating the thousands
+    */
+
+    num = Math.abs(num)
+    num = num.toFixed(2)
+
+    numSplit = num.split(".")
+
+    int = numSplit[0]
+
+    if (int.length > 3) {
+      int = int.substr(0, int.length - 3) + "," + int.substr(int.length - 3, 3) // 2310 => 2,310
+    }
+
+    dec = numSplit[1]
+
+    return (type === "exp" ? "-" : "+") + " " + int + '.' + dec
+  }
+
   return {
     getInput: function () {
       return {
@@ -170,7 +194,7 @@ var UIController = (function () {
       // Replace the placeholder text with some actual data
       newHtml = html.replace("%id%", obj.id)
       newHtml = newHtml.replace("%description%", obj.description)
-      newHtml = newHtml.replace("%value%", obj.value)
+      newHtml = newHtml.replace("%value%", formatNumber(obj.value, type))
 
       // Insert the HTML into the D
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml)
@@ -202,10 +226,13 @@ var UIController = (function () {
     },
 
     displayBudget: function (obj) {
-      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget
-      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc
+      var type
+      obj.budget > 0 ? type = 'inc' : type = 'exp'
+
+      document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type)
+      document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc')
       document.querySelector(DOMstrings.expensesLabel).textContent =
-        obj.totalExp
+      formatNumber(obj.totalExp, 'exp')
       document.querySelector(DOMstrings.percentageLabel).textContent =
         obj.percentage
 
@@ -228,11 +255,10 @@ var UIController = (function () {
 
       nodeListForEach(fields, function (current, index) {
         if (percentages[index] > 0) {
-          current.textContent = percentages[index] + '%'
+          current.textContent = percentages[index] + "%"
         } else {
-          current.textContent = '---'
+          current.textContent = "---"
         }
-
       })
     },
 
